37471dfc9cfda7d7bc4f9654e338fb15cf962697
nosorae <nosorae@naver.com>
2021-10-30 20:01:32 +0900

    :construction: rule 화면 구현

---
+++ app/src/main/java/com/abouttime/blindcafe/presentation/onboarding/agreement/AgreementFragment.kt
@@ -0,0 +1,6 @@
+package com.abouttime.blindcafe.presentation.onboarding.agreement
+
+import androidx.fragment.app.Fragment
+
+class AgreementFragment: Fragment() {
+}
--- app/src/main/java/com/abouttime/blindcafe/presentation/onboarding/rule/RuleFragment.kt
+++ app/src/main/java/com/abouttime/blindcafe/presentation/onboarding/rule/RuleFragment.kt
@@ -1,7 +1,6 @@
 package com.abouttime.blindcafe.presentation.onboarding.rule
 
 import android.os.Bundle
-import android.util.LayoutDirection
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.widget.ViewPager2
@@ -9,16 +8,24 @@ import com.abouttime.blindcafe.R
 import com.abouttime.blindcafe.databinding.FragmentRuleBinding
 import com.abouttime.blindcafe.presentation.onboarding.rule.adapter.RuleVpAdapter
 
-class RuleFragment: Fragment(R.layout.fragment_rule) {
+class RuleFragment : Fragment(R.layout.fragment_rule) {
     private var binding: FragmentRuleBinding? = null
 
-    private val viewPagerCallback = object: ViewPager2.OnPageChangeCallback() {
+    private val viewPagerCallback = object : ViewPager2.OnPageChangeCallback() {
         override fun onPageSelected(position: Int) {
-            when(position) {
-                1 -> binding?.tvCafeRuleDescription?.setText(R.string.rule_1)
-                2 -> binding?.tvCafeRuleDescription?.setText(R.string.rule_2)
-                3 -> binding?.tvCafeRuleDescription?.setText(R.string.rule_3)
-                4 -> binding?.tvCafeRuleDescription?.setText(R.string.rule_4)
+            when (position) {
+                0 -> {
+                    binding?.tvCafeRuleDescription?.setText(R.string.rule_1)
+                }
+                1 -> {
+                    binding?.tvCafeRuleDescription?.setText(R.string.rule_2)
+                }
+                2 -> {
+                    binding?.tvCafeRuleDescription?.setText(R.string.rule_3)
+                }
+                3 -> {
+                    binding?.tvCafeRuleDescription?.setText(R.string.rule_4)
+                }
             }
             super.onPageSelected(position)
         }
@@ -33,25 +40,31 @@ class RuleFragment: Fragment(R.layout.fragment_rule) {
 
     }
 
-    private fun initViewPager(fragmentRuleBinding: FragmentRuleBinding) =  with(fragmentRuleBinding) {
-        val adapter = RuleVpAdapter(
-            listOf(
-                R.drawable.bg_rule1,
-                R.drawable.bg_rule2,
-                R.drawable.bg_rule3,
-                R.drawable.bg_rule4
+    private fun initViewPager(fragmentRuleBinding: FragmentRuleBinding) =
+        with(fragmentRuleBinding) {
+            val adapter = RuleVpAdapter(
+                listOf(
+                    R.drawable.bg_rule_1,
+                    R.drawable.bg_rule_2,
+                    R.drawable.bg_rule_3,
+                    R.drawable.bg_rule_4
+                )
             )
-        )
 
-        vpImageContainer.adapter = adapter
-        diIndicator.setViewPager2(vpImageContainer)
+            vpImageContainer.adapter = adapter
+            diIndicator.setViewPager2(vpImageContainer)
 
-        vpImageContainer.registerOnPageChangeCallback(viewPagerCallback)
 
+        }
+
+    override fun onStart() {
+        super.onStart()
+        binding?.vpImageContainer?.registerOnPageChangeCallback(viewPagerCallback)
     }
 
-    override fun onPause() {
+    override fun onStop() {
+        super.onStop()
         binding?.vpImageContainer?.unregisterOnPageChangeCallback(viewPagerCallback)
-        super.onPause()
+
     }
 }
