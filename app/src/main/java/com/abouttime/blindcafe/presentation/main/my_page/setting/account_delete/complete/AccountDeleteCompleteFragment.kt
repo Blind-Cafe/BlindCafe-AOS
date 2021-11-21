f2256b6f25f60f88b2c29c3276452eca871217c0
nosorae <nosorae@naver.com>
2021-11-21 11:44:08 +0900

    :lipstick: 계정탈퇴완료 화면 추가

---
+++ app/src/main/java/com/abouttime/blindcafe/presentation/main/my_page/setting/account_delete/complete/AccountDeleteCompleteFragment.kt
@@ -0,0 +1,11 @@
+package com.abouttime.blindcafe.presentation.main.my_page.setting.account_delete.complete
+
+import com.abouttime.blindcafe.R
+import com.abouttime.blindcafe.common.base.BaseFragment
+import org.koin.android.viewmodel.ext.android.viewModel
+
+class AccountDeleteCompleteFragment: BaseFragment<AccountDeleteCompleteViewModel>(R.layout.fragment_account_delete_complete) {
+    override val viewModel: AccountDeleteCompleteViewModel by viewModel()
+
+
+}
---
+++ app/src/main/java/com/abouttime/blindcafe/presentation/main/my_page/setting/account_delete/complete/AccountDeleteCompleteViewModel.kt
@@ -0,0 +1,6 @@
+package com.abouttime.blindcafe.presentation.main.my_page.setting.account_delete.complete
+
+import com.abouttime.blindcafe.common.base.BaseViewModel
+
+class AccountDeleteCompleteViewModel: BaseViewModel() {
+}
