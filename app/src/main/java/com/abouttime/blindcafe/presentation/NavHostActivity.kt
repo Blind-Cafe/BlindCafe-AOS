1d543063d62cb1a82c3859ca279cea2b90d19465
nosorae <nosorae@naver.com>
2021-12-06 10:38:46 +0900

    :sparkles: 캡쳐방지코드 추가

--- app/src/main/java/com/abouttime/blindcafe/presentation/NavHostActivity.kt
+++ app/src/main/java/com/abouttime/blindcafe/presentation/NavHostActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.util.Log
 import android.view.MotionEvent
 import android.view.View
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isGone
 import androidx.navigation.NavController
@@ -26,6 +27,8 @@ class NavHostActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setTheme(R.style.Theme_BlindCafe)
+
+        window.addFlags(WindowManager.LayoutParams.FLAG_SECURE)
         binding = ActivityNavHostBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
